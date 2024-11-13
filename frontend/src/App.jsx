549e6886117834c4a5447a4122c071104f75637a
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-13 20:00:11 +0200

    adding css files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import "./styles/base.css"
+import "./styles/colors.css"
+import "./styles/utilities.css"
+import "./styles/home.css"
 
 function App() {
   return (
---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import "./styles/base.css"
+import "./styles/colors.css"
+import "./styles/utilities.css"
+import "./styles/home.css"
+
+const App = () =>{
+
+
+    return ()
+}
