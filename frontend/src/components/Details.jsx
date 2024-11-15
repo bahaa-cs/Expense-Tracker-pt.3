79fe13d8d29e8b1da4ebfdec308f16e128062fc7
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-15 03:04:15 +0200

    fixing jsx structure in mapping

--- frontend/src/components/Details.jsx
+++ frontend/src/components/Details.jsx
@@ -15,26 +15,34 @@ const Details = () => {
 
 
   const listTransactions = transactions.map((transaction) => (
-    <div key={transaction.id}>
-      <div className="flex row Titles green-txt">
-        <div className="price-title green-txt">Price</div>
-        <div className="type-title green-txt">Type</div>
-        <div className="date-title green-txt">Date</div>
-        <div className="notes-title green-txt">Notes</div>
-        <div className="actions-title green-txt">Actions</div>
-      </div>
 
-      <div className="flex column data-info black-txt" id="data-info">
-        <div className="price-title green-txt">{transaction.price}</div>
-        <div className="type-title green-txt">{transaction.type}</div>
-        <div className="date-title green-txt">{transaction.date}</div>
-        <div className="notes-title green-txt">{transaction.notes}</div>    
+
+      <div className="flex row data black-txt" id="data-info">
+        <div key={transaction.id}></div>
+        <div className="price">{transaction.price}</div>
+        <div className="type">{transaction.type}</div>
+        <div className="date">{transaction.date}</div>
+        <div className="notes">{transaction.notes}</div>    
+        <div class="flex row action-btns black-txt">
+                <button class="edit-btn action-btn black-txt">edit</button>
+                <button class="delete-btn action-btn red-bg white-txt">delete</button>
+            </div>
       </div>
-    </div>
+    
   ));
 
   return (
     <section className="flex column transaction-info primary-bg" id="transaction-info">
+    
+      <div className="flex row Titles green-txt">
+        <div className="price-title">Price</div>
+        <div className="type-title">Type</div>
+        <div className="date-title">Date</div>
+        <div className="notes-title">Notes</div>
+        <div className="actions-title">Actions</div>
+      </div>
+      
+
       {listTransactions}
     </section>
   );
