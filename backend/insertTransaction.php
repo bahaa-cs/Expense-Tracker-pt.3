0fc00f16ac9b37ae9d38ac04696bb84990267c80
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-16 12:34:54 +0200

    backend <> adding empty string validation

--- backend/insertTransaction.php
+++ backend/insertTransaction.php
@@ -9,17 +9,22 @@ $notes = $_POST['notes'];
 $users_id = $_POST['users_id'] ?? 1;
 
 
+if( empty($price) || empty($type) || empty($date) || empty($notes)){
+    echo json_encode(["message"=>"Null values"]);
+}
+else{
 
-$query = $connection->prepare(
-    "INSERT INTO transactions (price, type , date, notes, users_id)
-VALUES (?,?,?,?, ?)"
-);
-$query->bind_param("isssi", $price,$type,$date,$notes, $users_id);
-
-if ($query) {
-    if ($query->execute())
-        echo json_encode(["message"=>"Inserted successfully"]);
-    else
-        echo json_encode(["message"=>"Error executing insert query: " . $query->error]);
-} else
-    echo json_encode(["message"=>"Error preparing insert query: ".$connect->error]);
+    $query = $connection->prepare(
+        "INSERT INTO transactions (price, type , date, notes, users_id)
+    VALUES (?,?,?,?, ?)"
+    );
+    $query->bind_param("isssi", $price,$type,$date,$notes, $users_id);
+    
+    if ($query) {
+        if ($query->execute())
+            echo json_encode(["message"=>"Inserted successfully"]);
+        else
+            echo json_encode(["message"=>"Error executing insert query: " . $query->error]);
+    } else
+        echo json_encode(["message"=>"Error preparing insert query: ".$connect->error]);
+}
